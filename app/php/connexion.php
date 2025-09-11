ce27d91ac4e884a4e15f78da698f073e51f953c3
samiaelb <samia.elbouche9293@gmail.com>
2025-09-11 15:45:39 +0200

    deuxieme version

--- app/php/connexion.php
+++ app/php/connexion.php
@@ -1,24 +1,23 @@
 <?php
-
 session_start();
 require_once __DIR__ . "/config.php";
 
-
 /** @var PDO $pdo */
-
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = htmlspecialchars($_POST["email"]);
     $mot_de_passe = $_POST["mot_de_passe"];
     $role = $_POST["role"];
 
-
-    $sql = "SELECT * FROM eleves WHERE email = :email
-    UNION 
-    SELECT * FROM professeurs WHERE email = :email
-    LIMIT 1";
-
-
+    if ($role === "eleve") {
+        $sql = "SELECT * FROM eleves WHERE email = :email LIMIT 1";
+        $redirect = "/php/profileleve.php";
+    } elseif ($role === "professeur") {
+        $sql = "SELECT * FROM professeurs WHERE email = :email LIMIT 1";
+        $redirect = "/php/profilprof.php";
+    } else {
+        die("Rôle non reconnu !");
+    }
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute([":email" => $email]);
@@ -26,15 +25,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     if ($user) {
         if (password_verify($mot_de_passe, $user["mot_de_passe"])) {
-
             $_SESSION["user_id"] = $user["id"];
             $_SESSION["user_nom"] = $user["nom"];
             $_SESSION["user_prenom"] = $user["prenom"];
             $_SESSION["role"] = $role;
 
-            header('Location: /php/home.php');
+            header("Location: $redirect");
             exit;
-
         } else {
             $error = "Mot de passe incorrect.";
         }
@@ -43,10 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     if (isset($error)) {
-        echo "<p> " . $error . "</p>";
+        echo "<p>$error</p>";
     }
-
-
 }
-
 include_once "../html/connexion.html";
